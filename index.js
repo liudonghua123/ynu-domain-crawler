3e638ae01bbabae3dd36e815854f0430870388fe
liudonghua123 <liudonghua123@gmail.com>
2020-10-15 16:54:50 +0800

    update code to support platform chrome

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer-core');
+const os = require("os");
+const path = require("path");
 const fs = require('fs/promises');
 const ora = require('ora');
 
@@ -54,10 +56,37 @@ function transformShortenDomain(domains) {
   return results;
 }
 
+function getChromePath() {
+  let browserPath;
+
+  if (os.type() === "Windows_NT") {
+    // Chrome is usually installed as a 32-bit application, on 64-bit systems it will have a different installation path.
+    const programFiles =
+      os.arch() === "x64"
+        ? process.env["PROGRAMFILES(X86)"]
+        : process.env.PROGRAMFILES;
+    browserPath = path.join(
+      programFiles,
+      "Google/Chrome/Application/chrome.exe"
+    );
+  } else if (os.type() === "Linux") {
+    browserPath = "/usr/bin/google-chrome";
+  } else if (os.type() === "Darwin") {
+    browserPath =
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+  }
+
+  if (browserPath && browserPath.length > 0) {
+    return path.normalize(browserPath);
+  }
+
+  throw new TypeError(`Cannot run action. ${os.type} is not supported.`);
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+    executablePath: getChromePath(),
   });
   const [page] = await browser.pages();
   await page.setRequestInterception(true);
@@ -91,14 +120,13 @@ function transformShortenDomain(domains) {
     }
   }
   await browser.close();
-  // remove dumplicate
   console.info(`got all domains ${results.length}, ${results}`);
   results = [...new Set(results)];
   results.sort();
-  writeFile('output-all.txt', results.join('\n'));
+  await writeFile('output-all.txt', results.join('\n'));
   console.info(`got unique domains ${results.length}, ${results}`);
   results = transformShortenDomain(results);
   console.info(`got unique sorted shorten domains ${results.length}, ${results}`);
-  writeFile('output-shorten.txt', results.join('\n'));
-  writeFile('output-shorten-squid.txt', results.map(item => `.${item}`).join('\n'));
+  await writeFile('output-shorten.txt', results.join('\n'));
+  await writeFile('output-shorten-squid.txt', results.map(item => `.${item}`).join('\n'));
 })();
